1ba1b30769ca279eb6bb25a3124713d32dc806b4
joelramos <joel.m.ramos@me.com>
2015-04-04 16:43:38 -0700

    Removed trailing white space from problem16.py

--- problem16.py
+++ problem16.py
@@ -2,12 +2,12 @@
 
 '''
 * * * Problem 16 * * *
-* 
+*
 * __author__ = 'j4ramos'
 * __date__   = '3/13/2015'
 *
 * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-* 
+*
 * What is the sum of the digits of the number 2^1000?
 '''
 
